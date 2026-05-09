eaae68230a10fab991a2443a1f210c0cb026703d
Mofeleng <mofelengmokgope@gmail.com>
2026-05-09 08:01:51 +0200

    Added checks to confirm that user is editing their own songs

--- backend/Api/Controllers/JobsController.cs
+++ backend/Api/Controllers/JobsController.cs
@@ -5,6 +5,7 @@ using Api.Services;
 using Hangfire;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Api.Controllers
 {
@@ -14,11 +15,13 @@ namespace Api.Controllers
     {
         private readonly SongService _songService;
         private readonly ISongRepository _songRepository;
+        private readonly IUserRepository _userRepository;
 
-        public JobsController(SongService songService, ISongRepository songRepository)
+        public JobsController(SongService songService, ISongRepository songRepository, IUserRepository userRepository)
         {
             _songService = songService;
             _songRepository = songRepository;
+            _userRepository = userRepository;
         }
 
         [HttpPost]
@@ -26,10 +29,17 @@ namespace Api.Controllers
         [Authorize]
         public ActionResult GenerateSong([FromBody] GenerateSongClientRequest request)
         {
-            
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userIdClaim == null) return Unauthorized();
+            if (userIdClaim != request.UserId) return BadRequest();
+
+            var user = _userRepository.GetById(Guid.Parse(request.UserId));
+            if (user == null) return NotFound();
+
             var song = new Song
             {
-                UserId = Guid.Parse(request.UserId),
+                UserId = user.Id,
                 Prompt = request.Prompt,
                 Lyrics = request.Lyrics,
                 LyricsDescription = request.LyricsDescription,
@@ -49,7 +59,7 @@ namespace Api.Controllers
 
             GenerateSongDto songProps = new GenerateSongDto
             {
-                UserId = Guid.Parse(request.UserId),
+                UserId = user.Id,
                 SongId = newSong.Id
 
             };
